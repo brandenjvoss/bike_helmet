5f4b242448a5710358f6f3cc5ef9ed43235d015a
Jpentney <jpentney@telus.net>
2022-06-10 12:18:33 -0700

    setup 1.0

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,9 +1,53 @@
 #include <Arduino.h>
 #include <HUSKYLENS.h>
 #include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+
+HUSKYLENS huskylens;
+//HUSKYLENS green line >> SDA; blue line >> SCL
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 
 void setup() {
   // put your setup code here, to run once:
+
+ Serial.begin(115200);
+ 
+//-------------------------------------------------------------------------------------------------
+//DISPLAYS
+
+ 
+
+
+ 
+  //-------------------------------------------------------------------------------------------------
+  //HUSKYLENS
+
+  Wire.begin();
+      while (!huskylens.begin(Wire))
+      {
+          Serial.println(F("Begin failed!"));
+          Serial.println(F("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>>I2C)"));
+          Serial.println(F("2.Please recheck the connection."));
+          delay(100);
+      }
+
+      huskylens.writeAlgorithm(ALGORITHM_OBJECT_TRACKING); //Switch the algorithm to object tracking.
+
+
 }
 
 void loop() {
